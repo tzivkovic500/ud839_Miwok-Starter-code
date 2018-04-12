91df844f6d2dd478a10f9073cfee7cd78f5a7118
tea <tzivkovic500@gmail.com>
2018-04-12 16:56:52 +0200

    modify the WordAdapter: to accept colors as third input, to set the correct background color on each list item. Modify the category activities to call the new WordAdapter constructor

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -1,6 +1,8 @@
 package com.example.android.miwok;
 
 import android.app.Activity;
+import android.content.Context;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,12 +18,17 @@ import java.util.ArrayList;
 
 public class WordAdapter extends ArrayAdapter<Word> {
 
-    public WordAdapter(Activity context, ArrayList<Word> words) {
-        // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
-        // the second argument is used when the ArrayAdapter is populating a single TextView.
-        // Because this is a custom adapter for two TextViews and an ImageView, the adapter is not
-        // going to use this second argument, so it can be any value. Here, we used 0.
-        super(context, 0, words);
+    /** Resource ID for the background color for this list of words */
+    private int mColorResourceId;
+
+        public WordAdapter(Context context, ArrayList<Word> words, int colorResourceId) {
+            // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
+            // the second argument is used when the ArrayAdapter is populating a single TextView.
+            // Because this is a custom adapter for two TextViews and an ImageView, the adapter is not
+            // going to use this second argument, so it can be any value. Here, we used 0.
+
+            super(context, 0, words);
+            mColorResourceId = colorResourceId;
     }
     public View getView(int position, View convertView, ViewGroup parent) {
         // Check if the existing view is being reused, otherwise inflate the view
@@ -30,6 +37,8 @@ public class WordAdapter extends ArrayAdapter<Word> {
             listItemView = LayoutInflater.from(getContext()).inflate(
                     R.layout.list_item, parent, false);
         }
+
+
 
         // Get the {@link Word} object located at this position in the list
        Word currentWord = getItem(position);
@@ -58,6 +67,13 @@ public class WordAdapter extends ArrayAdapter<Word> {
                    // Otherwise hide the ImageView (set visibility to GONE)
                    mImageView.setVisibility(View.GONE);
                     }
+
+        // Set the theme color for the list item
+        View textContainer = listItemView.findViewById(R.id.text_container);
+        // Find the color that the resource ID maps to
+        int color = ContextCompat.getColor(getContext(), mColorResourceId);
+        // Set the background color of the text container View
+                textContainer.setBackgroundColor(color);
 
         // Return the whole list item layout (containing 2 TextViews and an ImageView)
         // so that it can be shown in the ListView
