f239d5415b8ea24fafc8af20789f2c5bb93c44f6
tea <tzivkovic500@gmail.com>
2018-07-13 17:26:32 +0200

    donottranslate.xml, WordAdapter

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -1,16 +1,16 @@
 package com.example.android.miwok;
 
 import android.app.Activity;
-import android.content.Context;
-import android.support.v4.content.ContextCompat;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.TextView;
+        import android.content.Context;
+        import android.support.v4.content.ContextCompat;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.ArrayAdapter;
+        import android.widget.ImageView;
+        import android.widget.TextView;
 
-import java.util.ArrayList;
+        import java.util.ArrayList;
 
 /**
  * Created by Tea on 24.3.2018..
